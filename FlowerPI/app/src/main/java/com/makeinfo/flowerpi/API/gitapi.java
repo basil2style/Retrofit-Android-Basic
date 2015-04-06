fd66d29e9b269caa70ab33799c462181cfff5e1e
Basil <basil2style@gmail.com>
2015-04-06 23:20:09 +0530

    created by basil

--- FlowerPI/app/src/main/java/com/makeinfo/flowerpi/API/gitapi.java
+++ FlowerPI/app/src/main/java/com/makeinfo/flowerpi/API/gitapi.java
@@ -6,9 +6,7 @@ import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
-/**
- * Created by Gokul Balakrishnan on 3/24/2015.
- */
+
 public interface gitapi {
     @GET("/users/{user}")
    public void getFeed(@Path("user") String user,Callback<gitmodel> response);
